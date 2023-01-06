049df3fedd1cb87d883832d6c5c5d77e9107c1af
ChinoCoralalde <coraldefabianchino@gmail.com>
2023-01-06 13:10:32 +0800

    installation of packages

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, View, Box } from "native-base";
+import React from 'react'
+import Home from './src/Home'
 
-export default function App() {
+const BlackBackground = ({ children }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View bgColor="black." flex={1}>
+      {children}
     </View>
   );
+};
+
+const App = () => {
+  return (
+    <NativeBaseProvider>
+      <BlackBackground>
+   <Home></Home>
+      </BlackBackground>
+    </NativeBaseProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
